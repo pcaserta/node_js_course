583001b634a17fe5f263ddcde4204f85cd647559
pcaserta <patrick.caserta@live.longwood.edu>
2021-05-26 17:18:13 -0400

    products

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
 
-const server = -http.createServer(app);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-server.listen(3000);
+app.get("/favicon.ico", (req, res) => res.sendStatus(204));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action ='/product' method='POST'> <input type='text' name='title'></input><button type='submit'>Add Product</button></form>"
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body.title);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<form> hello from express! </form>");
+});
+
+app.listen(3000);
